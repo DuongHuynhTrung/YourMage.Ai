aeeb46f3b801c474718c47de9ea24f0567a3f892
HieuNghia <email_mi@domain.com>
2023-10-13 00:37:30 +0700

    Change gitignore

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import { IoAdapter } from '@nestjs/platform-socket.io';
-// import * as http from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,8 +26,5 @@ async function bootstrap() {
 
   await app.listen(5000);
   Logger.log(`Application listening on port 5000`);
-
-  // const httpServer = http.createServer(app.getHttpAdapter().getInstance());
-  // app.useWebSocketAdapter(new IoAdapter(httpServer));
 }
 bootstrap();
